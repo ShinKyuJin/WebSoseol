7c441d072e2f30dea5e9439655372210a52b3bad
miminishin <miminishin@naver.com>
2019-06-08 19:49:40 +0900

    Register Success

---
+++ Login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="/css/Login.css">
+  </head>
+  <body>
+    <form class="LoginForm" action="index.html" method="post">
+            <input type="text" name="userID" placeholder="ID">
+            <input type="password" name="userPassword" value="Password">
+            <input type="submit" name="Loginbtn" value="Login">
+    </form>
+    <a href="./Register.php">회원가입</a>
+  </body>
+</html>
---
+++ Register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form method="post">
+      <input type="text" name="userID" placeholder="ID">
+      <input type="password" name="userPassword" placeholder="Password">
+      <input type="text" name="userName" placeholder="Name">
+      <input type="submit" value="submit">
+      <?php
+        $con = mysqli_connect('miminishin.cafe24.com','miminishin','s7731731','miminishin');
+        if($con) {
+          echo "hello";
+        }else {
+          echo mysqli_connect_errno() , ":";
+        }
+        $userID = $_POST['userID'];
+        $userPassword = $_POST['userPassword'];
+        $userName = $_POST['userName'];
+        $sql = "INSERT INTO USER(userID,userPassword,userName) VALUES('$userID','$userPassword','$userName')";
+        $result = mysqli_query($con,$sql);
+
+       ?>
+    </form>
+  </body>
+</html>
