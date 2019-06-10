746889d3578f3fca7a7dd0dbdf9d9af371f8e3ae
miminishin <miminishin@naver.com>
2019-06-10 15:12:11 +0900

    helloeveryone

--- Register.php
+++ Register.php
@@ -1,25 +1,9 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <form method="post">
-      <input type="text" name="userID" placeholder="ID">
-      <input type="password" name="userPassword" placeholder="Password">
-      <input type="text" name="userName" placeholder="Name">
-      <input type="submit" value="submit">
-      <?php
-        $con = mysqli_connect('miminishin.cafe24.com','miminishin','s7731731','miminishin');
-
-        $userID = $_POST['userID'];
-        $userPassword = $_POST['userPassword'];
-        $userName = $_POST['userName'];
-        $sql = "INSERT INTO USER(userID,userPassword,userName) VALUES('$userID','$userPassword','$userName')";
-        $result = mysqli_query($con,$sql);
-        
-       ?>
-    </form>
-  </body>
-</html>
+<?php
+  $con = mysqli_connect('miminishin.cafe24.com','miminishin','s7731731','miminishin');
+  $userID = $_POST['userID'];
+  $userPassword = $_POST['userPassword'];
+  $userName = $_POST['userName'];
+  $sql = "INSERT INTO USER(userID,userPassword,userName) VALUES('$userID','$userPassword','$userName')";
+  $result = mysqli_query($con,$sql);
+  header('Location:Login.php');
+?>
