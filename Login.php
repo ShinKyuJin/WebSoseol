475188d54ff13cf6b4981b774d73959370c9cf2a
miminishin <miminishin@naver.com>
2019-06-15 20:04:19 +0900

    PasswordEncryption

--- Login.php
+++ Login.php
@@ -1,30 +1,6 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="/css/Login.css">
-  </head>
-  <body>
-    <form class="LoginForm" action="index.html" method="post">
-            <input type="text" name="userID" placeholder="ID">
-            <input type="password" name="userPassword" placeholder="Password">
-            <button type="submit" name="button" onclick="login">Login</button>
-    </form>
-    <a href="./Register.php">회원가입</a>
-  </body>
-  <script type="text/javascript">
-    function login() {
-      <?php
-      $con = mysqli_connect('miminishin.cafe24.com','miminishin','s7731731','miminishin');
-      $userID = $_POST['userID'];
-      $userPassword = $_POST['userPassword'];
+<?php
+$con = mysqli_connect('miminishin.cafe24.com','miminishin','s7731731','miminishin');
+$userID = $_POST['userID'];
+$userPassword = $_POST['userPassword'];
 
-      $result = $mysqli->query($check);
-      if($check->num_rows==1) {
-
-      }
-       ?>
-    }
-  </script>
-</html>
+ ?>
--- Register.php
+++ Register.php
@@ -3,7 +3,15 @@
   $userID = $_POST['userID'];
   $userPassword = $_POST['userPassword'];
   $userName = $_POST['userName'];
-  $sql = "INSERT INTO USER(userID,userPassword,userName) VALUES('$userID','$userPassword','$userName')";
+
+  $encrypted_password = password_hash($userPassword, PASSWORD_DEFAULT);
+
+  $sql = "INSERT INTO USER(userID,userPassword,userName) VALUES('$userID','$encrypted_password','$userName')";
   $result = mysqli_query($con,$sql);
-  header('Location:Login.php');
+  if($result) {
+    echo '<script>alert("Success");location.replace("SignIn.html");</script>';
+  }
+  else {
+    echo '<script>alert("Fail");</script>';
+  }
 ?>
