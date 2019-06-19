3c575d0c60069cd373e8bd08694f39b7b3f0f0df
miminishin <miminishin@naver.com>
2019-06-19 17:23:11 +0900

    LoginForm

--- Login.php
+++ Login.php
@@ -1,6 +1,9 @@
 <?php
-$con = mysqli_connect('miminishin.cafe24.com','miminishin','s7731731','miminishin');
-$userID = $_POST['userID'];
-$userPassword = $_POST['userPassword'];
+  $con = mysqli_connect('miminishin.cafe24.com','miminishin','s7731731','miminishin');
+  $userID = $_POST['userID'];
+  $userPassword = $_POST['userPassword'];
+  $sql = "SELECT * FROM USER WHERE userID = '$userID'";
+  $result = mysqli_query($con,$sql);
+
 
  ?>
