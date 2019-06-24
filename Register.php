1f0fb1ee653b316055ee6a48d0281c9d38cb135a
miminishin <miminishin@naver.com>
2019-06-24 16:14:00 +0900

    복구완료

--- Register.php
+++ Register.php
@@ -1,2 +1,15 @@
 <?php
- ?>
+  $con = mysqli_connect('miminishin.cafe24.com','miminishin','s7731731','miminishin');
+  $userID = $_POST['userID'];
+  $userPassword = $_POST['userPassword'];
+  $userName = $_POST['userName'];
+  $encrypted_password = password_hash($userPassword, PASSWORD_DEFAULT);
+  $sql = "INSERT INTO USER(userID,userPassword,userName) VALUES('$userID','$encrypted_password','$userName')";
+  $result = mysqli_query($con,$sql);
+  if($result) {
+    echo '<script>alert("Success");location.replace("SignIn.html");</script>';
+  }
+  else {
+    echo '<script>alert("Fail");</script>';
+  }
+?>
