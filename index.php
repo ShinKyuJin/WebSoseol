4573332bfe40e966065843da272c604022471415
miminishin <miminishin@naver.com>
2019-07-11 16:24:08 +0900

    test

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>웹소설</title>
+  </head>
+  <script type="text/javascript" src = "index.js">
+  </script>
+  <body>
+
+
+  </body>
+</html>
